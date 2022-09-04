daef5296f50f46b2b1c1cda95c9d228f1bd16db7
WillyPete2501 <kronos8765@gmail.com>
2022-09-04 13:43:31 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,88 @@
+//create library variable
+const express = require('express');
+const {Pool} = require('pg');
+
+//create app variable
+const app = express();
+
+//create middlewares
+app.use(express.json());
+//app.use(express.static('public'))
+
+//create PORT variable
+const PORT = 3000;
+
+//create Pool variable
+const pool = new Pool ({
+    user: 'krono',
+    password: 'f',
+    host: 'localhost',
+    port: 5432,
+    database: 'scratchdb'
+});
+
+//create listening port
+app.listen(PORT, () => {
+    console.log(`${PORT} check`)
+})
+
+//get all
+app.get('/scratch', async (request, response) => {
+    try {
+        const showAll = await pool.query('SELECT * FROM scratch');
+        response.status(200).send(showAll.rows)
+    } catch (error) {
+        response.send(error.message);
+    }
+})
+
+// //get one
+// app.get('/scratch/:id', async (request, response) => {
+//     try {
+//         const {id} = request.params;
+//         const {rows} = await pool.query('SELECT * FROM scratch WHERE id = $1', [id]);
+//         response.status(200).send(rows);
+//     } catch (error) {
+//         response.send(error.message);
+//     }
+// });
+
+
+// //delete one
+// app.delete('/scratch/:id', async (request, response) => {
+//     try {
+//         const {id} = request.params;
+//         const {rows} = await pool.query('DELETE FROM scratch WHERE id = $1', [id]);
+//         response.status(200).send(rows);
+//     } catch (error) {
+//         response.send(error.message);
+//     }
+// })
+
+// //create one
+// app.post('/scratch', async (request, response) => {
+//     try {
+//         const {id} = request.params;
+//         const {name, quantity, date_made, rating} = request.body;
+//         const {rows} = await pool.query('INSERT INTO scratch (name, quantity, date_made, rating) VALUES ($1, $2, $3, $4)', [name, quantity, date_made, rating]);
+//         const showOne = await pool.query('SELECT * FROM scratch WHERE id = $1', [id]);
+//         //console.log(rows)
+//         response.status(200).send(showOne.rows);
+//     } catch (error) {
+//         response.send(error.message);
+//     }
+// });
+
+// //patch one
+// app.patch('/scratch/:id', async (request, response) => {
+//     try {
+//         const {id} = request.params;
+//         const {name, quantity, date_made, rating} = request.body;
+//         const {rows} = await pool.query('UPDATE scratch SET name = $1, quantity = $2, date_made = $3, rating = $4 WHERE id = $5', [name, quantity, date_made, rating, id]);
+//         const showOne = await pool.query('SELECT * FROM scratch WHERE id = $1', [id]);
+//         //console.log(rows)
+//         response.status(200).send(showOne.rows);
+//     } catch (error) {
+//         response.send(error.message);
+//     }
+// })
