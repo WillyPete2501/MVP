0f5c65b61f1a77dc2da5ce3f448d87e2733bd2b1
WillyPete2501 <kronos8765@gmail.com>
2022-09-04 15:08:41 -0700

    Updated table

--- seed.sql
+++ seed.sql
@@ -4,8 +4,8 @@ CREATE TABLE users (
     f_name varchar(15),
     l_name varchar(20),
     height_inches integer,
-    weight_lbs integer
+    weight_lbs integer,
 );
 
-INSERT INTO users (f_name, l_name, height_inches, weight_lbs) VALUES ('Tom', 'Powers', 73, 220);
-INSERT INTO users (f_name, l_name, height_inches, weight_lbs) VALUES ('Natalee', 'Kotos', 67, 110);
+INSERT INTO users (f_name, l_name, height_inches, weight_lbs, age) VALUES ('Tom', 'Powers', 73, 220, 36);
+INSERT INTO users (f_name, l_name, height_inches, weight_lbs, age) VALUES ('Natalee', 'Kotos', 67, 110, 28);
